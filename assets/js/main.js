5cd4fcddd5fd5e7cf832830ffec62229e5e3c8f3
Giacomo Cruz <giacomo.cruz@meetliquid.com>
2018-04-04 21:08:41 -0500

    Autocomplete and filters

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ $(function () {
     var $region;
     var $provincia;
     var $company;
+    var $suggest;
+    var $ruc;
 
     var populateRegion = function () {
         var tempHtml = [];
@@ -17,6 +19,7 @@ $(function () {
             tempHtml.push("<option value='" + item.departamento + "'>" + item.nombre + "</option>");
         });
         $region.html('<option value="" disabled selected>Departamento</option>' + tempHtml.join(''));
+        tempHtml = [];
     };
 
     var populateProvincia = function (event) {
@@ -29,15 +32,43 @@ $(function () {
             tempHtml.push("<option value='" + item.provincia + "'>" + item.nombre + "</option>");
         });
         $provincia.html('<option value="" disabled selected>Provincia</option>' + tempHtml.join(''));
+        tempHtml = [];
     };
 
-    var filterCompanies = function (event) {
-        var q = $(event.currentTarget).val();
+    var filterCompanies = function (q) {
         return companies.filter(function (elem) {
-            return Object.keys(elem).some(function (k) {
-                return elem[k].indexOf(q) !== -1;
+            return Object.keys(elem).some(function () {
+                return elem.empresa.toLowerCase().indexOf(q) !== -1;
             });
         });
+    };
+
+    var selectedOption = function (event) {
+        var $elem = $(event.currentTarget);
+        $ruc.val($elem.attr('data-ruc'));
+        $company.val($elem.text());
+    };
+
+    var typingInput = function (event) {
+        var q = $(event.currentTarget).val().toLowerCase();
+        var result;
+        var tempHtml = [];
+        if (q.length > 2) {
+            result = filterCompanies(q);
+        }
+
+        if (result !== undefined) {
+            result.forEach(function (item) {
+                tempHtml.push("<a href='#!'' data-ruc='" + item.ruc + "'' class='collection-item'>" + item.empresa + '</a>');
+            });
+            $suggest.html(tempHtml.join(''));
+            $suggest.slideDown();
+            $('a[data-ruc]').on('click', selectedOption);
+        } else {
+            $suggest.slideUp();
+        }
+
+        tempHtml = [];
     };
 
     var loadUbigeo = function () {
@@ -53,15 +84,29 @@ $(function () {
         });
     };
 
+    var hideSuggest = function () {
+        $suggest.slideUp();
+    };
+
+    var showSuggest = function () {
+        if ($suggest.html() !== '') {
+            $suggest.slideDown();
+        }
+    };
+
     var bind = function () {
         $region.on('change', populateProvincia);
-        $company.on('keyUp', filterCompanies);
+        $company.on('keyup', typingInput);
+        $company.on('blur', hideSuggest);
+        $company.on('focus', showSuggest);
     };
 
     var cache = function () {
         $region = $('#dep');
         $provincia = $('#prov');
         $company = $('#company');
+        $ruc = $('#ruc');
+        $suggest = $('#suggest');
     };
 
     var setup = function () {
